6b391ea85be3963a8a3c173d3e021d85df10791f
shero37 <ushimookani3712@gmail.com>
2023-02-22 00:28:27 +0900

    studentsコントローラーの修正

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
 class StudentsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  
+  before_action :authenticate_user!, only: [:index, :new, :create, :show]
+
   def index
     @students = Student.where(user_id: current_user.id).includes(:user).order("created_at DESC")
   end
